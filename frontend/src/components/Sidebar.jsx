6edce34ebb27f36277f0ebfe445f1ab4386deb77
KiruthikRajan-S <ad.tristoneindustries@gmail.com>
2026-04-02 15:23:01 +0530

    comment admin 2 side bar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,6 +1,5 @@
 import { Link, useLocation } from 'react-router-dom';
-import { Home, Users, BookOpen, Calendar, FileText, Bell, LogOut, ClipboardList, Award, Menu, X, UtensilsCrossed, Bus, Building2, User, Activity, MessageSquare, Monitor, FileCheck , DollarSign, AlertTriangle, Settings, MessageCircle, Briefcase, Book, Lightbulb } from 'lucide-react';
-import { Home, Users, BookOpen, Calendar, FileText, Bell, LogOut, ClipboardList, Award, Menu, X, UtensilsCrossed, Bus, Building2, User, Activity, MessageSquare, Monitor, FileCheck , DollarSign, AlertTriangle, Settings, MessageCircle, Briefcase, Book, BarChart2, Server, Upload, GraduationCap } from 'lucide-react';
+import { Home, Users, BookOpen, Calendar, FileText, Bell, LogOut, ClipboardList, Award, Menu, X, UtensilsCrossed, Bus, Building2, User, Activity, MessageSquare, Monitor, FileCheck , DollarSign, AlertTriangle, Settings, MessageCircle, Briefcase, Book, Lightbulb, BarChart2, Server, Upload, GraduationCap } from 'lucide-react';
 import { useContext, useState, useEffect } from 'react';
 import { AuthContext } from '../context/AuthContext';
 import { SettingsContext } from '../context/SettingsContext';
@@ -55,8 +54,8 @@ const Sidebar = () => {
     { to: '/admin/finance', icon: DollarSign, label: 'Finance Management' },
     { to: '/admin/fee-structure', icon: DollarSign, label: 'Fee Structure Builder' },
     { to: '/admin/payroll', icon: DollarSign, label: 'Payroll Management' },
-    { to: '/admin/academic-calendar', icon: Calendar, label: 'Academic Calendar' },
-    { to: '/admin/admissions', icon: GraduationCap, label: 'Admissions' },
+    //{ to: '/admin/academic-calendar', icon: Calendar, label: 'Academic Calendar' },
+    //{ to: '/admin/admissions', icon: GraduationCap, label: 'Admissions' },
     { to: '/admin/fee-scholarships', icon: DollarSign, label: 'Fee & Scholarships' },
     { to: '/admin/scholarships', icon: Award, label: 'Scholarships' },
     { to: '/admin/discipline', icon: AlertTriangle, label: 'Discipline Oversight' },
